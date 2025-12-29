778f5de1253efb72d76f8fd00cde5be228c7b174
tomiko <wtmk1713@gmail.com>
2025-12-29 17:37:13 +0900

    ログイン後の導線の作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # ログイン後に自動で/stocksへ飛ばす
+  def after_sign_in_path_for(resource)
+    stocks_path
+  end
 end
--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,4 +1,6 @@
 class StocksController < ApplicationController
+  before_action :authenticate_user! # 在庫一覧をログイン必須にする
+
   def index
   end
 end
